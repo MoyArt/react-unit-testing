a8aa968172e6f941f79f3f1f323d8fc74e9d3cc6
MoyArt <moyart@gmail.com>
2023-01-29 00:57:55 -0600

    feat(app.js): adding user state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import UserForm from './components/UserForm/UserForm';
-
+import { useState } from "react";
+import UserForm from "./components/UserForm/UserForm";
+import UserList from "./components/UserList/UserList";
 function App() {
+  const [users, setUsers] = useState([]);
+  const onUserAdd = (user) => {
+    console.log(user);
+    const updatedUsers = [...users, user];
+    setUsers(updatedUsers);
+  };
   return (
     <div>
-      <UserForm />
+      <UserForm onUserAdd={onUserAdd} />
+      <hr />
+      <UserList users={users} />
     </div>
   );
 }
